6f1e7eef4acb89b1ba9dc1a936ea6480340bba2d
Juan Ramirez <juanpram2000@gmail.com>
2023-12-16 21:53:31 -0800

    Fixing up the links site for testing

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -5,66 +5,71 @@ import {
   faMugHot,
 } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import CloduinaryImageWrapper from "../components/CloudinaryImageWraper";
+
+const HERO_PAGE_IMAGE_ID = "Tina/kzel2r28yhdyjahi2d51";
 
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Links() {
   return (
     <div className="flex flex-col items-center w-screen h-screen bg-gradient-to-br from-indigo-300 to-pink-300">
-      <div className="flex flex-col items-center justify-center w-full h-full max-w-lg gap-8">
-        <div className="relative flex flex-col items-center justify-center w-1/2 h-1/4">
-          <Image
-            src="/assets/images/linksPhoto.jpg"
-            alt=""
-            fill
-            className="object-cover object-top w-16 h-16 rounded-3xl"
+      <div className="flex flex-col items-center justify-center w-full h-full max-w-lg gap-4">
+        <figure className="relative w-2/3 h-2/4 ">
+          <CloduinaryImageWrapper
+            src={HERO_PAGE_IMAGE_ID}
+            alt="Tina Hero Section photo"
+            className="object-cover rounded-xl"
           />
-        </div>
-        <p className="text-4xl font-semibold tracking-tight">Tina Dinh</p>
+        </figure>
+
+        <h1 className="text-4xl font-semibold tracking-tight">Tina Dinh</h1>
+        <p className="text-lg text-center w-72">
+          Model with a passion for displaying beauty elegantly
+        </p>
         <ul className="flex flex-col w-3/4 max-w-5xl gap-4 text-2xl">
-          <li className="px-2 py-1 rounded-lg bg-slate-100">
+          <li className="px-2 py-1 rounded-2xl bg-slate-100">
             <Link
               href={"https://www.instagram.com/tinavivix/"}
-              className="flex flex-row items-center justify-between"
+              className="flex flex-row items-center justify-between text-center"
               aria-label="Instagram Link"
               target="_blank"
             >
-              Modeling Insta
               <FontAwesomeIcon icon={faInstagram} className="w-7 h-7" />
+              <p className="grow">Instagram</p>
             </Link>
           </li>
-          <li className="px-2 py-1 rounded-lg bg-slate-100">
+          <li className="px-2 py-1 rounded-2xl bg-slate-100">
             <Link
               href={"https://www.instagram.com/tinavivix/"}
-              className="flex flex-row items-center justify-between"
+              className="flex flex-row items-center justify-between text-center"
               aria-label="Instagram Link"
               target="_blank"
             >
-              Portfolio Site
               <FontAwesomeIcon icon={faLink} className="w-7 h-7" />
+              <p className="grow">Portfolio Site</p>
             </Link>
           </li>
-          <li className="px-2 py-1 rounded-lg bg-slate-100">
+          <li className="px-2 py-1 rounded-2xl bg-slate-100">
             <Link
               href={"https://www.instagram.com/tinavivix/"}
-              className="flex flex-row items-center justify-between"
+              className="flex flex-row items-center justify-between text-center"
               aria-label="Instagram Link"
               target="_blank"
             >
-              Calendly
               <FontAwesomeIcon icon={faCalendarDays} className="w-7 h-7" />
+              <p className="grow">Calendly</p>
             </Link>
           </li>
-          <li className="px-2 py-1 rounded-lg bg-slate-100">
+          <li className="px-2 py-1 rounded-2xl bg-slate-100">
             <Link
               href={"https://www.instagram.com/tinavivix/"}
-              className="flex flex-row items-center justify-between"
+              className="flex flex-row items-center justify-between text-center"
               aria-label="Instagram Link"
               target="_blank"
             >
-              Buy Me A Coffee
               <FontAwesomeIcon icon={faMugHot} className="w-7 h-7" />
+              <p className="grow">Buy Me A Coffee</p>
             </Link>
           </li>
         </ul>
