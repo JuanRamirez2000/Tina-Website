2b2b4f038f247003867923ce7752335b753ad9e5
Juan Ramirez <juanpram2000@gmail.com>
2023-12-17 20:54:13 -0800

    trying to restructure gallery stuff

--- app/components/ImageGrid.tsx
+++ app/components/ImageGrid.tsx
@@ -11,17 +11,19 @@ export default function ImageGrid({ images }: { images: SearchResults[] }) {
     return images.filter((_, idx) => idx % MAX_COLS === coldIndex);
   };
   return (
-    <div className="grid grid-cols-4 gap-2">
+    <div className="grid w-screen h-screen grid-cols-4 gap-2">
       {[getColumns(0), getColumns(1), getColumns(2), getColumns(3)].map(
         (column, idx) => (
-          <div className="flex flex-col gap-2" key={idx}>
+          <div className="flex flex-col w-full h-full gap-2" key={idx}>
             {column.map((image) => (
-              <CloduinaryImageWrapper
-                src={image.public_id}
-                sizes="100vw"
-                alt="Tina Hero Page"
-                key={image.public_id}
-              />
+              <figure key={image.public_id} className="relative w-1/3 h-full">
+                <CloduinaryImageWrapper
+                  src={image.public_id}
+                  alt="Tina Hero Page"
+                  key={image.public_id}
+                  className="object-cover rounded-xl"
+                />
+              </figure>
             ))}
           </div>
         )
