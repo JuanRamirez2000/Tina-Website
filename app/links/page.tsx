49943c041101da1767994276239bb966bf89ea96
Juan Ramirez <juanpram2000@gmail.com>
2023-12-19 19:03:14 -0800

    Going to work on styling

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -13,98 +13,117 @@ import Link from "next/link";
 
 export default function Links() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-auto bg-gradient-to-br from-indigo-300 to-pink-300">
-      <div className="flex flex-col items-center justify-center w-full h-screen max-w-lg gap-4 min-h-fit">
-        <figure className="relative w-2/3 h-1/2 grow-0">
+    <div className="flex flex-col items-center justify-center bg-gradient-to-br from-indigo-300 to-pink-300">
+      <div className="flex flex-col items-center justify-center w-full max-w-lg min-h-screen gap-2 py-20 overflow-y-auto md:py-0 h-fit">
+        <figure className="relative w-2/3 h-96 grow-0">
           <CloduinaryImageWrapper
             src={HERO_PAGE_IMAGE_ID}
             alt="Tina Hero Section photo"
             className="object-cover rounded-xl"
           />
         </figure>
-        <div className="flex flex-col items-center w-full gap-6 h-1/4">
+        <div className="flex flex-col items-center w-full gap-5 h-fit">
           <h1 className="text-4xl font-semibold tracking-tight">Tina Dinh</h1>
-          <p className="text-lg text-center w-72">
-            Model with a passion for displaying beauty elegantly
-          </p>
-          <ul className="flex flex-col w-3/4 max-w-5xl gap-4 text-2x">
-            <li className="px-3 py-1 rounded-2xl bg-slate-100">
+          {/* 
+
+<p className="text-lg text-center w-72">
+Model with a passion for displaying beauty elegantly
+</p>
+*/}
+          <ul className="flex flex-col w-3/4 max-w-5xl gap-4 text-2xl">
+            <li className="px-3 py-1 transition-all rounded-2xl bg-slate-100 hover:scale-105">
               <Link
                 href={"https://www.instagram.com/tinavivix/"}
-                className="flex flex-row items-center justify-between text-center"
+                className="flex flex-row items-center justify-between text-center "
                 aria-label="Instagram Link"
                 target="_blank"
               >
-                <FontAwesomeIcon icon={faInstagram} className="w-8 h-8" />
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  className="w-10 h-10 p-1.5 rounded-xl text-slate-50 bg-gradient-to-bl from-purple-500 via-red-600 to-amber-400"
+                />
                 <div className="flex flex-col grow">
                   <p className="text-xl font-semibold">Instagram</p>
-                  <p className="text-sm text-slate-600">
-                    My modeling instagram
-                  </p>
+                  <p className="text-sm text-slate-600">Modeling Instagram</p>
                 </div>
               </Link>
             </li>
-            <li className="px-3 py-1 rounded-2xl bg-slate-100">
+            <li className="px-3 py-1 transition-all rounded-2xl bg-slate-100 hover:scale-105">
               <Link
-                href={"https://www.instagram.com/tinavivix/"}
-                className="flex flex-row items-center justify-between text-center"
+                href={"https://tina-website-g52a.vercel.app/"}
+                className="flex flex-row items-center justify-between text-center "
                 aria-label="Instagram Link"
                 target="_blank"
               >
-                <FontAwesomeIcon icon={faLink} className="w-8 h-8" />
+                <FontAwesomeIcon
+                  icon={faLink}
+                  className="w-10 h-10 p-1.5 rounded-xl bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500 text-white"
+                />
                 <div className="flex flex-col grow">
                   <p className="text-xl font-semibold">Portfolio</p>
                   <p className="text-sm text-slate-600">
-                    Portfolio site for all my modeling
+                    Portfolio Site For All My Work
                   </p>
                 </div>
               </Link>
             </li>
-            <li className="px-3 py-1 rounded-2xl bg-slate-100">
+            <li className="px-3 py-1 transition-all rounded-2xl bg-slate-100 hover:scale-105">
               <Link
                 href={"https://www.instagram.com/tinavivix/"}
-                className="flex flex-row items-center justify-between text-center"
+                className="flex flex-row items-center justify-between text-center "
                 aria-label="Instagram Link"
                 target="_blank"
               >
-                <FontAwesomeIcon icon={faCalendarDays} className="w-8 h-8" />
+                <FontAwesomeIcon
+                  icon={faCalendarDays}
+                  className="w-10 h-10 p-1.5 rounded-xl bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500 text-slate-50"
+                />
                 <div className="flex flex-col grow">
                   <p className="text-xl font-semibold">Calendly</p>
                   <p className="text-sm text-slate-600">Book Me!</p>
                 </div>
               </Link>
             </li>
-            <li className="px-3 py-1 rounded-2xl bg-slate-100">
+            <li className="px-3 py-1 transition-all rounded-2xl bg-slate-100 hover:scale-105">
               <Link
-                href={"https://www.instagram.com/tinavivix/"}
-                className="flex flex-row items-center justify-between text-center"
+                href={
+                  "https://www.amazon.com/hz/wishlist/ls/229375X9AS1M9?ref_=wl_share"
+                }
+                className="flex flex-row items-center justify-between text-center "
                 aria-label="Instagram Link"
                 target="_blank"
               >
-                <FontAwesomeIcon icon={faAmazon} className="w-8 h-8" />
+                <FontAwesomeIcon
+                  icon={faAmazon}
+                  className="w-10 h-10 p-1.5 rounded-xl bg-gradient-to-br from-blue-400 to-emerald-400 text-slate-50"
+                />
                 <div className="flex flex-col grow">
                   <p className="text-xl font-semibold">Amazon Wishlist</p>
-                  <p className="text-sm text-slate-600">Come buy me a gift</p>
+                  <p className="text-sm text-slate-600">Buy Me A Gift</p>
                 </div>
               </Link>
             </li>
-            <li className="px-3 py-1 rounded-2xl bg-slate-100">
+            <li className="px-3 py-1 transition-all rounded-2xl bg-slate-100 hover:scale-105">
               <Link
                 href={"https://www.instagram.com/tinavivix/"}
-                className="flex flex-row items-center justify-between text-center"
+                className="flex flex-row items-center justify-between text-center "
                 aria-label="Instagram Link"
                 target="_blank"
               >
-                <FontAwesomeIcon icon={faMugHot} className="w-8 h-8" />
+                <FontAwesomeIcon
+                  icon={faMugHot}
+                  className="w-10 h-10 p-1.5 rounded-xl bg-gradient-to-br from-cyan-200 to-cyan-400 text-slate-50"
+                />
                 <div className="flex flex-col grow">
-                  <p className="text-xl font-semibold">Come buy me a coffee</p>
+                  <p className="text-xl font-semibold">Come Buy Me A Coffee</p>
                   <p className="text-sm text-slate-600">
-                    Always great to support
+                    Always Great To Support
                   </p>
                 </div>
               </Link>
             </li>
           </ul>
+          <h2 className="text-sm text-slate-700">Made with love by @Juan</h2>
         </div>
       </div>
     </div>
