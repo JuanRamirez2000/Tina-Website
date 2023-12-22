c481d4f68ed510adae3a12b828feb51801131e35
Juan Ramirez <juanpram2000@gmail.com>
2023-12-22 09:40:31 -0800

    restructuring of app. Removed aboutme section as well

--- app/(portfolioPage)/Navbar.tsx
+++ app/(portfolioPage)/Navbar.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function Navbar({ children }: { children: React.ReactNode }) {
   return (
-    <div className="drawer">
+    <div className="fixed drawer">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="flex flex-col drawer-content">
         {/* Navbar */}
-        <div className="w-full px-5 text-white bg-pink-300 lg:px-10 navbar">
+        <div className="w-full px-5 text-slate-700 bg-slate-300 lg:px-10 navbar">
           <div className="flex-none lg:hidden">
             <label
               htmlFor="my-drawer-3"
@@ -20,36 +20,38 @@ export default function Navbar({ children }: { children: React.ReactNode }) {
           <div className="flex-1 text-xl font-semibold">Tina Dinh</div>
           <div className="flex-none hidden lg:block">
             <ul className="text-lg menu menu-horizontal">
-              <li>
+              <li className="btn btn-ghost hover:bg-sky-400">
                 <Link href={"/"}>Home</Link>
               </li>
-              <li>
-                <Link href={"/aboutme"}>About Me</Link>
-              </li>
-              <li>
-                <details className="dropdown">
-                  <summary>Gallery</summary>
-                  <ul className="p-2 shadow menu dropdown-content z-[1] bg-pink-300 rounded-lg w-32 text-lg">
-                    <li>
+              <li className="btn btn-ghost hover:bg-sky-400 ">
+                <div className="dropdown dropdown-bottom">
+                  <div tabIndex={0} role="button">
+                    Gallery
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="p-2 shadow menu dropdown-content z-[1] text-slate-700 bg-slate-300 rounded-box w-32 "
+                  >
+                    <li className="transition-all rounded-lg hover:scale-110 hover:bg-sky-400">
                       <Link href={"/gallery?contentType=street"}>Street</Link>
                     </li>
-                    <li>
+                    <li className="transition-all rounded-lg hover:scale-110 hover:bg-sky-400">
                       <Link href={"/gallery?contentType=portraits"}>
                         Portaits
                       </Link>
                     </li>
-                    <li>
+                    <li className="transition-all rounded-lg hover:scale-110 hover:bg-sky-400">
                       <Link href={"/gallery?contentType=boudoir"}>
                         Boudouir
                       </Link>
                     </li>
                   </ul>
-                </details>
+                </div>
               </li>
-              <li>
+              <li className="btn btn-ghost hover:bg-sky-400">
                 <Link href={"/rates"}>Rates</Link>
               </li>
-              <li>
+              <li className="btn btn-ghost hover:bg-sky-400">
                 <Link href={"/links"}>Links</Link>
               </li>
             </ul>
@@ -64,12 +66,9 @@ export default function Navbar({ children }: { children: React.ReactNode }) {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="min-h-full p-4 text-lg text-white bg-pink-300 menu w-72">
+        <ul className="min-h-full p-4 text-lg text-slate-700 bg-slate-300 menu w-72">
           <li>
             <Link href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link href={"/aboutme"}>About Me</Link>
           </li>
           <li>
             <details open>
--- app/(portfolioPage)/page.tsx
+++ app/(portfolioPage)/page.tsx
@@ -1,5 +1,21 @@
 import CloduinaryImageWrapper from "../components/CloudinaryImageWraper";
 
+const HERO_PAGE_IMAGE_ID = "Tina/sh194qz8a88nyu0mkay6";
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col w-full h-screen lg:flex-row">
+      <section className="w-full h-1/2 lg:h-full lg:w-2/3">
+        <h1>Tina Dinh</h1>
+      </section>
+      <div className="flex flex-col items-center justify-center w-full h-1/2 lg:h-full lg:w-1/3">
+        <figure className="relative h-full lg:w-full lg:h-full">
+          <CloduinaryImageWrapper
+            src={HERO_PAGE_IMAGE_ID}
+            alt="Tina Hero Section photo"
+            className="object-cover object-bottom rounded-lg lg:rounded-none"
+          />
+        </figure>
+      </div>
+    </main>
+  );
 }
