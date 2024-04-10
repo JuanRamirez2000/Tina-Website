92ec59fcd7e60d294d2f419c977cb76ebe2407c3
Juan Ramirez <juanpram2000@gmail.com>
2024-04-10 12:44:15 -0700

    style: added a backdrop and the model name on top of the home image:

--- app/(portfolioPage)/page.tsx
+++ app/(portfolioPage)/page.tsx
@@ -10,14 +10,24 @@ const TINA_BOUDOIR_THUMBNAIL = "Tina/tkq0frt7w1qwcllmsku1";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center w-full px-5 lg:px-10 h-fit py-14 lg:h-fit">
-      <section className="relative w-full max-w-screen-3xl h-[42rem]">
-        <CloduinaryImageWrapper
-          src={HERO_PAGE_IMAGE_ID}
-          fill={true}
-          alt=""
-          className="object-cover py-10"
-        />
+    <main className="flex flex-col items-center w-full px-5 lg:px-10 h-fit lg:h-fit">
+      <section className="w-full pt-32 max-w-screen-3xl h-fit">
+        <div className="relative w-full h-[42rem]">
+          <CloduinaryImageWrapper
+            src={HERO_PAGE_IMAGE_ID}
+            fill={true}
+            alt=""
+            className="object-cover"
+          />
+          <div className="absolute inset-0 z-10 opacity-100 bg-pink-400/20 ">
+            <div className="absolute z-20 top-1/4 left-[15%]">
+              <h2 className="text-6xl font-bold text-pink-50">Tina Dinh</h2>
+            </div>
+            <div className="absolute z-20 bottom-1/4 right-[15%] ">
+              <h2 className="text-6xl font-bold text-pink-50">Model</h2>
+            </div>
+          </div>
+        </div>
       </section>
       <section className="flex flex-col items-center justify-around w-4/5 max-w-screen-3xl lg:flex-row h-96">
         <h1 className="text-5xl font-bold tracking-tight">Comp Card</h1>
