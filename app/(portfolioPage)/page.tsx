4b7bf6a1c1e8d28d6b648fb39d10de515902a02d
Juan Ramirez <juanpram2000@gmail.com>
2024-04-09 21:44:04 -0700

    refactor: re-styled the homepage to fit a nicer theme

--- app/(portfolioPage)/page.tsx
+++ app/(portfolioPage)/page.tsx
@@ -1,60 +1,97 @@
 import Link from "next/link";
 import CloduinaryImageWrapper from "../components/CloudinaryImageWraper";
 
-const HERO_PAGE_IMAGE_ID = "tina-website-39_ncgjkh";
+const HERO_PAGE_IMAGE_ID = "Tina/odxnrqgyzpbp2xpr5qve";
+
+const TINA_STREET_THUMBNAIL = "Tina/kzel2r28yhdyjahi2d51";
+const TINA_PORTRAIT_THUMBNAIL = "Tina/odo4vy9x1vmfivfxdwbs";
+const TINA_STUDIO_THUMBNAIL = "Tina/hi44jqyqn2lozf3fkati";
+const TINA_BOUDOIR_THUMBNAIL = "Tina/tkq0frt7w1qwcllmsku1";
+
 export default function Home() {
   return (
-    <main className="flex flex-col-reverse items-center justify-around w-full py-24 h-fit lg:h-full lg:py-0 lg:flex-row">
-      <section className="flex flex-col items-center justify-center w-full min-h-fit lg:h-full lg:w-2/3">
-        <div className="flex flex-col items-center w-full max-w-3xl gap-4 h-fit ">
-          <h2 className="text-3xl font-semibold">Tina Dinh</h2>
-          <p className="max-w-xl px-4 text-center">
-            Hey! I&apos;m Tina! <br /> A full time student and hobbyist model.{" "}
-            <br /> I got into modeling as an attempt to show natural beauty and
-            elegance through many forms of media. My main focus right now is
-            photoshoots.
-            <br /> Most of my work revolves around Street, Portait, and Boudoir
-            modeling, however I am open to other forms of modeling if the time
-            and shoot fit my style. <br /> This site was made to showcase my
-            work. Lets shoot sometime!
-          </p>
-          <div className="flex flex-col items-center justify-center w-64 h-32 p-4 rounded-lg bg-gradient-to-br from-indigo-300 to-pink-300">
-            <ul className="flex flex-row gap-6 ">
-              <li>
-                <h3 className="text-sm font-semibold">Height</h3>
-                <p className="text-3xl font-bold">5&apos;4&quot;</p>
-              </li>
-              <li>
-                <h3 className="text-sm font-semibold ">Bust</h3>
-                <p className="text-3xl font-bold">34&quot;</p>
-              </li>
-              <li>
-                <h3 className="text-sm font-semibold ">Waist</h3>
-                <p className="text-3xl font-bold">28&quot;</p>
-              </li>
-            </ul>
-          </div>
-          <h2 className="text-sm text-slate-700">
-            Made with love by{" "}
-            <Link
-              href={"https://www.juanr.dev"}
-              className="text-blue-600 underline dark:text-blue-500 hover:no-underline"
-            >
-              @Juan
-            </Link>
-          </h2>
-        </div>
+    <main className="flex flex-col items-center w-full px-5 lg:px-10 h-fit py-14 lg:h-fit">
+      <section className="relative w-full h-[42rem]">
+        <CloduinaryImageWrapper
+          src={HERO_PAGE_IMAGE_ID}
+          fill={true}
+          alt=""
+          className="object-cover py-10"
+        />
       </section>
-      <div className="flex flex-col items-center justify-center w-full h-96 lg:h-full lg:w-1/3">
-        <figure className="relative w-3/4 h-full lg:w-full grow-0">
-          <CloduinaryImageWrapper
-            src={HERO_PAGE_IMAGE_ID}
-            alt="Tina Hero Section photo"
-            fill={true}
-            className="object-cover rounded-lg lg:rounded-none"
-          />
-        </figure>
-      </div>
+      <section className="flex flex-col items-center justify-around w-4/5 max-w-screen-lg lg:flex-row h-96">
+        <h1 className="text-5xl font-bold tracking-tight">Comp Card</h1>
+        <ul className="text-xl">
+          <li>Height: 5&apos;4&quot;</li>
+          <li>Bust: 34&quot;</li>
+          <li>Waist: 28&quot;</li>
+        </ul>
+      </section>
+
+      <section className="w-full h-full">
+        <ul className="flex flex-col gap-16 lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:justify-items-center size-full">
+          <li className="relative w-full lg:w-3/4 h-[52rem] cursor-pointer">
+            <CloduinaryImageWrapper
+              src={TINA_STREET_THUMBNAIL}
+              fill={true}
+              alt=""
+              className="object-cover"
+            />
+            <div className="absolute inset-0 z-10 flex items-end justify-center duration-300 opacity-0 hover:opacity-100 bg-gradient-to-t from-cyan-300/[99%] pb-20">
+              <p className="text-3xl font-semibold text-cyan-50">
+                Street Modeling
+              </p>
+            </div>
+          </li>
+          <li className="relative w-full lg:w-3/4 h-[52rem] cursor-pointer">
+            <CloduinaryImageWrapper
+              src={TINA_PORTRAIT_THUMBNAIL}
+              fill={true}
+              alt=""
+              className="object-cover"
+            />
+            <div className="absolute inset-0 z-10 flex items-end justify-center duration-300 opacity-0 hover:opacity-100 bg-gradient-to-t from-indigo-300/[99%] pb-20">
+              <p className="text-3xl font-semibold text-indigo-50">
+                Portrait Modeling
+              </p>
+            </div>
+          </li>
+          <li className="relative w-full lg:w-3/4 h-[52rem] cursor-pointer">
+            <CloduinaryImageWrapper
+              src={TINA_STUDIO_THUMBNAIL}
+              fill={true}
+              alt=""
+              className="object-cover"
+            />
+            <div className="absolute inset-0 z-10 flex items-end justify-center duration-300 opacity-0 hover:opacity-100 bg-gradient-to-t from-pink-300/[99%] pb-20">
+              <p className="text-3xl font-semibold text-pink-50">
+                Studio Modeling
+              </p>
+            </div>
+          </li>
+          <li className="relative w-full lg:w-3/4 h-[52rem] cursor-pointer">
+            <CloduinaryImageWrapper
+              src={TINA_BOUDOIR_THUMBNAIL}
+              fill={true}
+              alt=""
+              className="object-cover"
+            />
+            <div className="absolute inset-0 z-10 flex items-end justify-center duration-300 opacity-0 hover:opacity-100 bg-gradient-to-t from-rose-300/[99%] pb-20">
+              <p className="text-3xl font-semibold text-rose-50">
+                Boudoir Modeling
+              </p>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <section className="flex flex-col items-center justify-center w-full py-24">
+        <p>
+          Made with love by{" "}
+          <Link href={"https://www.juanr.dev"} className="text-pink-400">
+            @Juan
+          </Link>
+        </p>
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,4 @@ module.exports = {
     },
   },
   plugins: [],
-  daisyui: {
-    themes: [],
-  },
 };
