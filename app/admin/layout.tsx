c83f57e69fe4aa19b3c1d83ab86eb5d297c3a0c4
Juan Ramirez <juanpram2000@gmail.com>
2024-04-11 14:14:30 -0700

    fix: fixed an error with the build of the site

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
-export default function Layout({
-  children,
-  photoModal,
-}: {
-  children: React.ReactNode;
-  photoModal: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="relative w-full h-screen">
       <div className="w-full h-fit">
         <Navbar />
       </div>
       <div className="w-full h-full">{children}</div>
-      {photoModal}
     </main>
   );
 }
